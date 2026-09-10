628919a5339444d34d2f33abd515fb2a3582c1fb
Stefan <3224372+SteGab@users.noreply.github.com>
2026-09-10 17:28:29 +0200

    Add 'fastfetch' and 'fd' to installation packages

--- toolbox/setup-toolbox.sh
+++ toolbox/setup-toolbox.sh
@@ -1,4 +1,4 @@
-packages=("zsh" "eza" "zoxide" "fzf" "bat" "ranger")
+packages=("zsh" "eza" "zoxide" "fzf" "bat" "ranger", "fastfetch", "fd")
 
 for p in "${packages[@]}"; do
   #echo "sudo dnf install -y $p"
