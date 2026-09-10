593c8b5a9317cdeeb5c0d1a86064b05b8ef64d80
Stefan <3224372+SteGab@users.noreply.github.com>
2026-09-10 17:29:42 +0200

    Remove 'sudo' from package installation command

--- toolbox/setup-toolbox.sh
+++ toolbox/setup-toolbox.sh
@@ -2,5 +2,5 @@ packages=("zsh" "eza" "zoxide" "fzf" "bat" "ranger", "fastfetch", "fd")
 
 for p in "${packages[@]}"; do
   #echo "sudo dnf install -y $p"
-  sudo dnf install -y $p
+  dnf install -y $p
 done
